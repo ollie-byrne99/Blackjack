11c59bea04c7b57f1fe45c276b90966594d6af4d
Robbie Stamp <stamprobbie@gmail.com>
2023-07-28 11:44:52 +0100

    just minor adjustments to syntax

--- index.js
+++ index.js
@@ -5,34 +5,33 @@ let rl = readline.createInterface({
     output: process.stdout
 });
 
-function cardValue() {
-    return Math.floor(Math.random() * 10) + 1;
-}
+const cardValue = () => Math.floor(Math.random() * 10) + 1;
+
 
 function playGame() {
     let playerTotal = cardValue() + cardValue();
     console.log(`You start with: ${playerTotal}`);
 
-function createDealerTotal() {
-    let dealerTotal = cardValue() + cardValue();
-    while(dealerTotal < playerTotal) {
-        dealerTotal += cardValue();
+    function createDealerTotal() {
+        let dealerTotal = cardValue() + cardValue();
+        while(dealerTotal < playerTotal) {
+            dealerTotal += cardValue();
+        }
+        return dealerTotal
     }
-    return dealerTotal
-}
-
-function whoWins(playerTotal,dealerTotal) {
-    if(dealerTotal > 21) {
-        console.log('Dealer busts! You win.');
-    } else if(dealerTotal > playerTotal) {
-        console.log('Dealer wins.');
-    } else if(dealerTotal < playerTotal) {
-        console.log('You win.');
-    } else {
-        console.log('It\'s a draw.');
+    
+    function whoWins(playerTotal,dealerTotal) {
+        if(dealerTotal > 21) {
+            console.log('Dealer busts! You win.');
+        } else if(dealerTotal > playerTotal) {
+            console.log('Dealer wins.');
+        } else if(dealerTotal < playerTotal) {
+            console.log('You win.');
+        } else {
+            console.log('It\'s a draw.');
+        }
+        rl.close();
     }
-    rl.close();
-}
 
     const question = () => {
         rl.question('Do you want to "hit" or "stay"? ', (answer) => {
