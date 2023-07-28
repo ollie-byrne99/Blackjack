b06827e395438bf314f93bb9509a3fa7a339f360
Robbie Stamp <stamprobbie@gmail.com>
2023-07-28 11:31:27 +0100

    created createDealerTotal and whoWins functions so that we're not repeating ourselves in the code

--- index.js
+++ index.js
@@ -13,7 +13,26 @@ function playGame() {
     let playerTotal = cardValue() + cardValue();
     console.log(`You start with: ${playerTotal}`);
 
-function dealerTotal() {}
+function createDealerTotal() {
+    let dealerTotal = cardValue() + cardValue();
+    while(dealerTotal < playerTotal) {
+        dealerTotal += cardValue();
+    }
+    return dealerTotal
+}
+
+function whoWins(playerTotal,dealerTotal) {
+    if(dealerTotal > 21) {
+        console.log('Dealer busts! You win.');
+    } else if(dealerTotal > playerTotal) {
+        console.log('Dealer wins.');
+    } else if(dealerTotal < playerTotal) {
+        console.log('You win.');
+    } else {
+        console.log('It\'s a draw.');
+    }
+    rl.close();
+}
 
     const question = () => {
         rl.question('Do you want to "hit" or "stay"? ', (answer) => {
@@ -25,44 +44,16 @@ function dealerTotal() {}
                     rl.close();
                 } else if (playerTotal === 21){
                     console.log("You got Blackjack!")
-                    let dealerTotal = cardValue() + cardValue();
-                    while(dealerTotal < playerTotal) {
-                        dealerTotal += cardValue();
-                    }
-    
+                    let dealerTotal = createDealerTotal();
                     console.log(`Dealer's total is: ${dealerTotal}`);
-                    
-                    if(dealerTotal > 21) {
-                        console.log('Dealer busts! You win.');
-                    } else if(dealerTotal > playerTotal) {
-                        console.log('Dealer wins.');
-                    } else if(dealerTotal < playerTotal) {
-                        console.log('You win.');
-                    } else {
-                        console.log('It\'s a draw.');
-                    }
-                    rl.close();
+                    whoWins(playerTotal,dealerTotal);
                 } else {
                     question();
                 }
             } else if(answer.toLowerCase() === 'stay') {
-                let dealerTotal = cardValue() + cardValue();
-                while(dealerTotal < playerTotal) {
-                    dealerTotal += cardValue();
-                }
-
+                let dealerTotal = createDealerTotal();
                 console.log(`Dealer's total is: ${dealerTotal}`);
-                
-                if(dealerTotal > 21) {
-                    console.log('Dealer busts! You win.');
-                } else if(dealerTotal > playerTotal) {
-                    console.log('Dealer wins.');
-                } else if(dealerTotal < playerTotal) {
-                    console.log('You win.');
-                } else {
-                    console.log('It\'s a draw.');
-                }
-                rl.close();
+                whoWins(playerTotal,dealerTotal);
             } else {
                 console.log('Please choose "hit" or "stay".');
                 question();
